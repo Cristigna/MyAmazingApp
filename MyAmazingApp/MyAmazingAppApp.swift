41dd9d83b16d283e335be7af1b02dda874c5bc31
Cristina Valenziano <valenziano.cristina@gmail.com>
2024-11-04 15:18:59 +0100

    Initial Commit

---
+++ MyAmazingApp/MyAmazingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmazingAppApp.swift
+//  MyAmazingApp
+//
+//  Created by Cristina Valenziano on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAmazingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
